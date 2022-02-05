f7183ecbaa16a05dfcacfbbee9fa47cb204c82c8
Phobos <86452055+0xPhobos@users.noreply.github.com>
2022-02-04 21:53:03 -0500

    Create Program.cmd

---
+++ Code/Program.cmd
@@ -0,0 +1,4 @@
+set "TwilioAccountSid=[TWILIO\_ACCOUNT\_SID]"
+set "TwilioAuthToken=[TWILIO\_AUTH\_TOKEN]"
+set "TwilioMessagingServiceSid=[TWILIO\_MESSAGING\_SERVICE\_SID]"
+set "ToPhoneNumber=[YOUR\_PHONE\_NUMBER]"
