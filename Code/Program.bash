852fde2637fe937a343cbfb77692a15612779bc5
Phobos <86452055+0xPhobos@users.noreply.github.com>
2022-02-04 21:51:32 -0500

    Create Program.bash

---
+++ Code/Program.bash
@@ -0,0 +1,4 @@
+export TwilioAccountSid=[TWILIO\_ACCOUNT\_SID]
+export TwilioAuthToken=TWILIO_AUTH_TOKEN]
+export TwilioMessagingServiceSid=[TWILIO\_MESSAGING\_SERVICE\_SID]
+export ToPhoneNumber=[YOUR\_PHONE\_NUMBER]
