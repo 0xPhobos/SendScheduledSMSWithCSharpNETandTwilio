060a2ab7a43ebc78c1fbd75a4365de80d2921935
Phobos <86452055+0xPhobos@users.noreply.github.com>
2022-02-04 21:52:40 -0500

    Create Program.ps1

---
+++ Code/Program.ps1
@@ -0,0 +1,4 @@
+$Env:TwilioAccountSid = "[TWILIO\_ACCOUNT\_SID]"
+$Env:TwilioAuthToken = "[TWILIO\_AUTH\_TOKEN]"
+$Env:TwilioMessagingServiceSid = "[TWILIO\_MESSAGING\_SERVICE\_SID]"
+$Env:ToPhoneNumber = "[YOUR\_PHONE\_NUMBER]"
