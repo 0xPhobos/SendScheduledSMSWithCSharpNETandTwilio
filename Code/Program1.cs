2bea656b91bb2576813d9eea15f76f2058aaa29e
Phobos <86452055+0xPhobos@users.noreply.github.com>
2022-02-04 21:41:25 -0500

    Create Program1.cs

---
+++ Code/Program1.cs
@@ -0,0 +1,19 @@
+using System;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+
+string twilioAccountSid = Environment.GetEnvironmentVariable("TwilioAccountSid");
+string twilioAuthToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
+string twilioMessagingServiceSid = Environment.GetEnvironmentVariable("TwilioMessagingServiceSid");
+string toPhoneNumber = Environment.GetEnvironmentVariable("ToPhoneNumber");
+
+TwilioClient.Init(twilioAccountSid, twilioAuthToken);
+
+var message = MessageResource.Create(
+    messagingServiceSid: twilioMessagingServiceSid,
+    body: "Sending SMS with Twilio and .NET is easy!",
+    to: new PhoneNumber(toPhoneNumber)
+);
+
+Console.WriteLine($"Message SID: {message.Sid}");
